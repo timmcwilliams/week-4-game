88e99f5c37a934bacbb8f41dd2ec35badafe3fc6
tmac4403 <drive911@gmail.com>
2017-10-10 20:20:56 -0500

    7

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,21 @@
-alert("hello");
-var crystal=5;
-var guess1=5;
+alert("je;;p");
 
-if guess1=5; {
-  alert("you win");
-}
-  else
-  {
-  alert("you lose");
-}
+$(document).ready(function() {
+	alert( "ready!" );
+$("#click-me").on("click", function() {
+alert("ive been clicked");
+
+// alert("hello");
+// var crystal=5;
+// var guess1=5;
+
+// function()
+
+// if guess1==5; {
+//   alert("you win");
+// }
+//   else
+
+//   alert("you lose");
+});
+});
