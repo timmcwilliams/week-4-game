9dfa4f10d220bfc68708a53b4522657cce8749a2
tmac4403 <drive911@gmail.com>
2017-10-13 16:04:01 -0500

    8

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,72 @@
-alert("je;;p");
+// start jscript
+// declare variables
+var crystalOne=Math.floor(Math.random() * 12) + 1;
+var crystalTwo=Math.floor(Math.random() * 12) + 1;
+var crystalThree=Math.floor(Math.random() * 12) + 1;
+var crystalFour =Math.floor(Math.random() * 12) + 1;
+var random = Math.floor(Math.random() * 101)+ 19;
+var guessTotal=0;
+var guess=0;
+var wins=0;
+var winsTotal=0;
+var loses=0;
+var losesTotal=0;
 
+function checkUserScore(){ 
+ 	if (guessTotal>random){
+		alert("you lose!");
+		losesTotal=losesTotal+1;
+		guessTotal=0;
+		// alert("you lost " + losesTotal)
+
+// adding to try and put everthing in a function
+$("#total").html("Total Points " + guessTotal);
+$("#loses").html("Games Lost " + losesTotal);
+$("#wins").html("Games Won " + winsTotal);
+
+
+	}	
+ 	else if (guessTotal==random){
+ 		// alert("you win
+ 		winsTotal=winsTotal+1;	
+		alert("you won!")
+		guessTotal=0;
+ 	}
+ 	}
+// doc ready function
 $(document).ready(function() {
-	alert( "ready!" );
-$("#click-me").on("click", function() {
-alert("ive been clicked");
-
-// alert("hello");
-// var crystal=5;
-// var guess1=5;
-
-// function()
-
-// if guess1==5; {
-//   alert("you win");
-// }
-//   else
-
-//   alert("you lose");
+	$("#random").html("Your Random Number " + random);
+	
+// assign a value to box 1 
+$("#box-1").on("click", function() {	
+		guessTotal=crystalOne + guessTotal;
+		$("#total").html("Total Points " + guessTotal);
+		$(checkUserScore);
+		$("#loses").html("Games Lost " + losesTotal);
+		$("#wins").html("Games Won " + winsTotal);
+});
+// assign a value to box 2 
+$("#box-2").on("click", function() {		
+		guessTotal=crystalTwo + guessTotal;
+		$("#total").html("Total Points " + guessTotal);
+		$(checkUserScore);
+		$("#loses").html("Games Lost " + losesTotal);
+		$("#wins").html("Games Won " + winsTotal);
+});	
+// assign a value to box 3 
+$("#box-3").on("click", function() {	
+		guessTotal=crystalThree + guessTotal;	
+		$("#total").html("Total Points " + guessTotal);		
+		$(checkUserScore);
+		$("#loses").html("Games Lost " + losesTotal);
+		$("#wins").html("Games Won " + winsTotal);
+});
+// assign a value to box 4 
+$("#box-4").on("click", function() {	
+		guessTotal=crystalFour + guessTotal;		
+		$("#total").html("Total Points " + guessTotal);
+		$(checkUserScore);
+		$("#loses").html("Games Lost " + losesTotal);
+		$("#wins").html("Games Won " + winsTotal);
 });
 });
